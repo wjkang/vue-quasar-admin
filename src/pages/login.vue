c5c55fc25c24063052a0c8c1e812e53d013ad812
tiago.bar <tiago.bar@openmailbox.org>
2018-10-14 15:26:21 -0300

    - Solved bug that was freezing login when using Firefox browser - Made a "cache" for OnMouseMove events - Added Translation with Vue-I18n

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,20 +6,20 @@
       <div class="column justify-center" style="width:300px">
         <div style="text-align:left">
           <!-- <q-field icon="account_circle" :error="$v.form.username.$error" error-label="Oops, we got an error.">
-            <q-input v-model="form.username" float-label="请输入账号" />
+            <q-input v-model="form.username" :float-label="$t('Please input Username')" />
           </q-field> -->
-          <q-input dark type="text" v-model="form.username" float-label="账号" clearable :error="$v.form.username.$error" :before="[{icon: 'account_circle'}]" autocomplete="new-password" />
+          <q-input dark type="text" v-model="form.username" :float-label="$t('Username')" clearable :error="$t($v.form.username.$error)" :before="[{icon: 'account_circle'}]" autocomplete="new-password" />
 
-          <q-input dark type="password" v-model="form.password" float-label="密码" clearable :error="$v.form.password.$error" :before="[{icon: 'vpn_key'}]" autocomplete="new-password" />
+          <q-input dark type="password" v-model="form.password" :float-label="$t('Password')" clearable :error="$t($v.form.password.$error)" :before="[{icon: 'vpn_key'}]" autocomplete="new-password" />
         </div>
         <div class="row justify-around q-mt-md">
           <q-btn color="primary" :loading="loading" @click="submit">
-            Login
+            {{$t('Login')}}
             <span slot="loading">
-              <q-spinner-hourglass class="on-left" /> Loading...
+              <q-spinner-hourglass class="on-left" /> {{$t('Loading')}}...
             </span>
           </q-btn>
-          <q-btn color="negative" @click="clear">Reset</q-btn>
+          <q-btn color="negative" @click="clear">{{$t('Reset')}}</q-btn>
         </div>
       </div>
     </div>
@@ -29,6 +29,7 @@
 <script>
 import THREE from "../libs/three/three";
 import { required } from "vuelidate/lib/validators";
+
 export default {
   data() {
     return {
@@ -63,7 +64,7 @@ export default {
       var windowHalfX = window.innerWidth / 2;
       var windowHalfY = window.innerHeight / 2;
       init();
-      this.interval = setInterval(loop, 1000 / 60);
+      this.interval = setInterval(loop, 1000 / 40);
       function init() {
         container = document.createElement("div");
         container.style.position = "relative";
@@ -90,9 +91,16 @@ export default {
         document.addEventListener("mousemove", onDocumentMouseMove, false);
         document.addEventListener("touchmove", onDocumentTouchMove, false);
       }
-      function onDocumentMouseMove(event) {
+      var mousemovetimer = null;
+      function onMouseMove(event) {
+        window.clearTimeout(mousemovetimer);
         mouseX = event.clientX - windowHalfX;
         mouseY = event.clientY - windowHalfY;
+        mousemovetimer = null;
+      }
+      function onDocumentMouseMove(event) {
+        if (!mousemovetimer)
+          mousemovetimer = window.setTimeout(onMouseMove, 50, event);
       }
       function onDocumentTouchMove(event) {
         if (event.touches.length == 1) {
@@ -103,7 +111,7 @@ export default {
       }
       function loop() {
         camera.position.x += (mouseX - camera.position.x) * 0.05;
-        //                    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+        // camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
         camera.position.y = 364;
         var i = 0;
         for (var ix = 0; ix < AMOUNTX; ix++) {
@@ -124,13 +132,17 @@ export default {
     submit() {
       this.$v.form.$touch();
       if (this.$v.form.$error) {
-        this.$q.notify("账号密码不能为空!");
+        this.$q.notify(this.$t("Account password cannot be empty") + '!');
         return;
       }
       this.loading = true;
       this.$store
         .dispatch("LoginByUserName", this.form)
         .then(() => {
+          if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+          }
           this.loading = false;
           this.$router.push({
             name: "home_index"
@@ -155,6 +167,7 @@ export default {
   }
 };
 </script>
+
 <style scoped>
 #login-app {
   background: none;
