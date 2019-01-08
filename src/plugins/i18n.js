345e7fa87ead607743c66793335a8099608bea25
wjk <977865769@qq.com>
2019-01-08 21:14:22 +0800

    Set i18n  zh-hans default

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -13,8 +13,8 @@ export default ({ app, router, store, Vue }) => {
   // new Vue({..., i18n: ... }).$mount(...)
 
   app.i18n = new VueI18n({
-    locale: 'en-us',    // zh-hans, en-us, pt-br
-    fallbackLocale: 'en-us',
+    locale: 'zh-hans',    // zh-hans, en-us, pt-br
+    fallbackLocale: 'zh-hans',
     messages
   })
 }
